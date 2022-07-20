f471298005620f6f7f3e4c8f86e8ed0f4f43d9c3
S-TIO <109645214+S-TIO@users.noreply.github.com>
2022-07-20 12:23:47 +0700

    Add files via upload

---
+++ WebSubmission/assets/function.js
@@ -0,0 +1,25 @@
+function navbarFunction() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
+
+function clickFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbutton')) {
+    var dropdown = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdown.length; i++) {
+      var openDropdown = dropdown[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
